1c247a06a512dbc76e1878a31039ca935c840851
JulioSDeveloper <106830991+JulioSDeveloper@users.noreply.github.com>
2022-10-27 21:28:32 -0300

    New version files

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const typed= new Typed('#typed',{
-strings:['Desarrollador Web'],stringsElement: '#cadenas-texto', // ID del elemento que contiene cadenas de texto a mostrar.
+strings:['Web Developer'],stringsElement: '#cadenas-texto', // ID del elemento que contiene cadenas de texto a mostrar.
         typeSpeed: 75, // Velocidad en mlisegundos para poner una letra,
         startDelay: 300, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
         backSpeed: 75, // Velocidad en milisegundos para borrrar una letra,
